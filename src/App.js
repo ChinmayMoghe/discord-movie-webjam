3c20c84dde99f5c5e56aa145094b4910d96a5394
Chinmay <chinmay11.moghe@gmail.com>
2022-03-26 16:56:36 +0530

    add base file changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+function Pyramid() {
+  return (
+    <mesh scale={[0.5, 0.5, 0.5]} rotation={[-360,65,0]}>
+      <coneBufferGeometry
+        attach='geometry'
+        args={[3.4, 3, 5, 1, false, 0, 6.3]}
+      />
+      <meshStandardMaterial
+        attach='material'
+        color='#000'
+        emissive='#000'
+        roughness={0.3}
+      />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Canvas>
+        <ambientLight intensity={1} color={0xff0000} />
+        <spotLight position={[20, -10, 0]} angle={0.3} color={0xff0000} />
+        <spotLight position={[10, -20, 0]} angle={0.3} color={0xff0000} />
+        <spotLight position={[30, 40, 0]} angle={0.3} color={0xff0000} />
+        <OrbitControls />
+        <Pyramid />
+      </Canvas>
     </div>
   );
 }
